239eda4efbc673c0f0b6c7c6f9fa9fe90907077a
RevengerZ <99681425+RevengerZ@users.noreply.github.com>
2022-07-06 09:49:08 +0530

    index.js
    code

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,21 @@
-*{
-margin: 0;
- padding: 0;
- box-sizing: border-box;
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale-1.0">
+<title>Contact Form To Email Using JavaScript</title>
+<link rel="stylesheet" href="websitecontact.css">
+</head>
 
-font-family: 'poppins', sans-serif;
+ <body>
+    <div class="container">
+<form onsubmit="sendEmail(); reset(); return false">
+    <h3>GET IN TOUCH</h3>
+    <input type="text" id="name" placeholder="Your Name" required>
+    <input type="email" id="email" placeholder="Your email ID" required>
+    <input type="text" id="phone" placeholder="Your phone no." required>
+    <textarea id="message" rows="4" placeholder="HOW CAN WE HELP YOU"></textarea>
+    <button type="submit">SEND</button>
+</form>
+</div>
 
-}
-
-.container{
-    width:100%;
-    height: 100vh;
-    background: #001660;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-
-}
-
-form{
-    background: #fff;
-    display: flex;
-    flex-direction: column;
-    padding: 2vw 4vw;
-    width: 90%;
-    max-width: 600px;
-    border-radius: 10px;
-
-
-}
-
-
-form h3{
-    color:#555 ;
-    font-weight: 800;
-    margin-bottom: 20px;
-}
-
-form input, form textarea{
-    border: 0;
-    margin: 10px 0;
-    padding: 20px;
-    outline: none;
-    background: #f5f5f5;
-    font-size: 16px
-}
-
-form button{
-    padding: 15px;
-    background: #ff5361;
-    color: #fff;
-    font-size: 18px;
-    border: 0;
-    outline: none;
-    cursor: pointer;
-    width: 150px;
-    margin: 20px auto 0;
-    border-radius: 30px;
-    
-}
+</body>
+</html>
